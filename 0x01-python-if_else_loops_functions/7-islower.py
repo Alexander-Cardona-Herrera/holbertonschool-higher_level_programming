0c4a1a99e48b7c664cf7d60723ad1c7f3fffcf08
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-04-27 18:57:40 -0500

    adding 7-islower.py file

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def islower(c):
+    for a in range(: len(str)):
+        if ord(str[a]) >= 97 and ord(str[a]) <= 122:
+            return (True)
+        else:
+            return (False)
