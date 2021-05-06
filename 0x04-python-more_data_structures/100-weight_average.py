b3f8e7a1bd63c872dd8ff241a224ed53a79b87a3
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-05-06 18:20:44 -0500

    0 condition

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list:
-
         up = (sum(i[0] * i[1] for i in my_list))
         down = (sum(i[1] for i in my_list))
         res = up / down
         return res
+    else:
+        return 0
