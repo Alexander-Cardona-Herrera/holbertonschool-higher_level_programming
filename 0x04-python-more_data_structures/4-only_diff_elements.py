d355dcc8c20da1979b68a7dd8d532fb827f7d07e
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-05-06 14:48:11 -0500

    some changes

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,4 +4,9 @@ def only_diff_elements(set_1, set_2):
         uncommon = list(set(set_1).symmetric_difference(set_2))
         return uncommon
     else:
-        return []
+        if not set_1:
+            return set_2
+        elif not set_1:
+            return set_1
+        else:
+            return []
