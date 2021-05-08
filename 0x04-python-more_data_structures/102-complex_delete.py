3de30d89870174d312b3f4721a7fda42546a8097
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-05-08 09:45:57 -0500

    adding 102-complex_delete.py file

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if value:
+        for x, y in list(a_dictionary.items()):
+            if y == value:
+                del a_dictionary[x]
+        return (a_dictionary)
+
+    else:
+        return a_dictionary
