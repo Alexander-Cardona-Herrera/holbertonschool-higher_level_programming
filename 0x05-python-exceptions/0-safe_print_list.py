9d310746feae02659e68e2cfa8a62317632777e1
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-05-17 12:40:25 -0500

    if my_ñist exist

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(0, x):
-            print("{}".format(my_list[i]), end='')
+    if (my_list):
+        try:
+            for i in range(0, x):
+                print("{}".format(my_list[i]), end='')
+
+        except (IndexError):
+            i = i - 1
+            pass
+
         print("")
         return i + 1
-    except (IndexError):
-        print("")
-        return i
-
