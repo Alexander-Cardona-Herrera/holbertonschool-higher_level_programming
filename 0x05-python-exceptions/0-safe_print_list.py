36a5e94f80da2050a697122bfda432b8d25276ee
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-05-17 12:33:58 -0500

    adding 1-safe_print_integer.py file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,6 @@ def safe_print_list(my_list=[], x=0):
         for i in range(0, x):
             print("{}".format(my_list[i]), end='')
         print("")
-
         return i + 1
     except (IndexError):
         print("")
