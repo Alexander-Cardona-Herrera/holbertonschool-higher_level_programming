03747169619327d200a28d1c067e8cd361c75949
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-06-02 00:00:00 -0500

    tryng somethings

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+defining the module.
+
+Use this class wizely.
+"""
+
+
+class MyInt(int):
+    """
+    Class MyInt.
+    """
+    def __bol__(self):
+        return False
---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,19 @@
+
+#!/usr/bin/python3
+"""
+For this function you have to put an object and a class as arguments.
+
+Use this function wizely.
+"""
+
+
+def add_attribute(mc, name, new_instance):
+    """
+    Object of an instance.
+    """
+
+    if type(mc) == class:
+        __new__(name[new_instance])
+
+    else:
+        raise TypeError["can't add new attribute"]
