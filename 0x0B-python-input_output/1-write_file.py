4a64f9d4643949dc685299a2977fbe4dbd01687a
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-06-02 17:19:19 -0500

    adding changes

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -10,5 +10,5 @@ def write_file(filename="", text=""):
     """
     Function that writes over a file
     """
-    with open(filename, mode="w+", encoding="UTF-8") as f:
+    with open(filename, "w+") as f:
         return f.write(text)
