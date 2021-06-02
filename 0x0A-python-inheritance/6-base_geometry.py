1d7531526404783d52fbbfa74e8ca2009ab3bcf7
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-06-01 21:56:20 -0500

    adding 6-base_geometry.py file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+For this function you have to put an object and a class as arguments.
+
+Use this function wizely.
+"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometry.
+    """
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
