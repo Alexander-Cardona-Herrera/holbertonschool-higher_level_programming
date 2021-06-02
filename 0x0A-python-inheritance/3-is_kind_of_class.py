d158986800276674216a77cf9ac77418ec362d55
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-06-01 21:17:10 -0500

    adding 3-is_kind_of_class.py file

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+For this function you have to put an object and a class as arguments.
+
+Use this function wizely.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Object of an instance.
+    """
+
+    if isinstance(obj, a_class):
+        return True
+
+    return False
