79d482c1d3a07b7e1e3e5f1eb291c6328069a3f4
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-06-01 22:13:32 -0500

    adding 7-base_geometry files

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+For this function you have to put an object and a class as arguments.
+
+Use this function wizely.
+"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometry.
+    """
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
