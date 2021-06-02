23c0a374f5c2ec36571df3b3c5e9d66b67eb4769
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-06-01 21:32:41 -0500

    adding 5base_geometry.py file

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+For this function you have to put an object and a class as arguments.
+
+Use this function wizely.
+"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometry.
+    """
+    pass
