848bbf2dd95ab0166fdf2c797209e04c7e20df3f
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-06-01 22:51:16 -0500

    adding 9-rectangle.py file

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+For this function you have to put an object and a class as arguments.
+
+Use this function wizely.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Subclass Rectangle.
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
