4d90f7dd7c70f9472dda1e7b523653a492d19ee8
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-06-01 20:02:21 -0500

    function dir

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+For this function tou have to put an object as an argument.
+
+use this function wizwly.
+"""
+
+
 def lookup(obj):
+    """
+    function for atributes.
+    """
+
     attributes = list(dir(obj))
 
     return attributes
