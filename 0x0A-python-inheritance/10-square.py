5584dfdd0de999756964e8359d07ea934b2ae9b9
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-06-01 23:01:41 -0500

    adding 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Defining the module.
+
+Use this function wizely.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Subclass Square.
+    """
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        return ("{}".format(self.__size * self.__size))
+    
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
