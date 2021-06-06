75656746b3d648e9663e04a8465435223f52a371
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-06-06 16:27:21 -0500

    adding 0x0C-python-almost_a_circle directory

---
+++ 0x0C-python-almost_a_circle/tests/unittester.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+** = __import__('*').**
+
+class TestStringMethods(unittest.TestCase):
+    
+    def test_max(self):
+        self.assertEqual()
+
+    def test_list(self):
+        self.assertIsNone()
+
+    def test_type(self):
+        self.assertIsInstance()
