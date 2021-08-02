5ed80e4d72ec6dc34cb36f87d07c29287643e07a
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-08-02 16:13:25 -0500

    lowercase

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* Task 6 */
 
-const myvar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const i in myvar) {
-  console.log(myvar[i]);
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const i in myVar) {
+  console.log(myVar[i]);
 }
