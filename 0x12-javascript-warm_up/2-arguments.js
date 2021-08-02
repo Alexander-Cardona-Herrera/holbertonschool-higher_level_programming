0ddc263ffe5aea421099de400c01dbc5d7921435
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-08-02 12:25:17 -0500

    adding corrections from task 0 and task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,11 +5,10 @@ const myargs0 = 'No argument';
 const myargs1 = 'Argument found';
 const myargs2 = 'Arguments found';
 
-if (process.argv.length == 2) {
-    console.log(myargs0);
-} else if (process.argv.length == 3) {
-    console.log(myargs1);
+if (process.argv.length === 2) {
+  console.log(myargs0);
+} else if (process.argv.length === 3) {
+  console.log(myargs1);
 } else {
-    console.log(myargs2);
+  console.log(myargs2);
 }
-
