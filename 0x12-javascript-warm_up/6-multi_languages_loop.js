6c15da54af3aca114286b3148a77f932c1799ef5
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-08-02 15:13:15 -0500

    trayng another statement for task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,6 +3,6 @@
 
 const myvar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (const i of myvar) {
-  console.log(i);
+for (let i = 0; i < myvar.length; i++) {
+  console.log(myvar[i]);
 }
