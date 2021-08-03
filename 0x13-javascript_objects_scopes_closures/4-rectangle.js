047e02826c946dfd6682a237f1087de2a85d4f93
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-08-03 12:15:05 -0500

    semistandar in task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,28 +2,31 @@
 /* Task 4 */
 
 class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    rotate() {
-        let temp = this.width;
-        this.width = this.height;
-        this.height = temp;
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
     }
-    double() {
-        this.width = this.width * 2;
-        this.height = this.height * 2;
-    }
-    print() {
-        let i = 0;
-        while (i < this.height) {
-            console.log('X'.repeat(this.width));
-            i++;
-        }
-    }
+  }
 }
 
 module.exports = Rectangle;
