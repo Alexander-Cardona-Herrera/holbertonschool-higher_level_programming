a10da8c5f221f8f8143262bb8c756b6555920548
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-08-04 13:53:42 -0500

    adding task 101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/* Task 101 */
+
+const newDict = require('./101-data').dict;
+
+const newDict2 = {};
+
+for (const key in newDict) {
+    if (!newDict2[newDict[key]]) {
+        newDict2[newDict[key]] = []
+    }
+    newDict2[newDict[key]].push(key)
+}
+
+console.log(newDict2);
