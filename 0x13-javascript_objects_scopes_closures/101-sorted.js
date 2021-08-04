be45fd859ea27e4571d31640abd35ddda1f4b4b4
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-08-04 13:56:21 -0500

    adding task 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -6,10 +6,10 @@ const newDict = require('./101-data').dict;
 const newDict2 = {};
 
 for (const key in newDict) {
-    if (!newDict2[newDict[key]]) {
-        newDict2[newDict[key]] = []
-    }
-    newDict2[newDict[key]].push(key)
+  if (!newDict2[newDict[key]]) {
+    newDict2[newDict[key]] = [];
+  }
+  newDict2[newDict[key]].push(key);
 }
 
 console.log(newDict2);
