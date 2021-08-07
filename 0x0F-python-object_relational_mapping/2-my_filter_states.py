9f0ffcd41549b59b2b93798855d57fd4330068ea
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-08-07 14:37:46 -0500

    adding double comprovation

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,8 @@ if __name__ == '__main__':
 
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if (row[1] == argv[4]):
+            print(row)
 
     cur.close()
     conn.close()
