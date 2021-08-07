0e5c673496cce9478e75f354249895bf8950978a
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-08-07 14:00:41 -0500

    minnpr changes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ from sys import argv
 if __name__ == '__main__':
 
     conn = MySQLdb.connect(host="localhost", port=3306,
-                           user=argv[1], password=argv[2],
+                           user=argv[1], passwd=argv[2],
                            db=argv[3], charset="utf8")
     cur = conn.cursor()
 
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,11 +10,12 @@ from sys import argv
 if __name__ == '__main__':
 
     conn = MySQLdb.connect(host="localhost", port=3306,
-                           user=argv[1], password=argv[2],
+                           user=argv[1], passwd=argv[2],
                            db=argv[3], charset="utf8")
     cur = conn.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}' \
+                ORDER BY id ASC".format(argv[4]))
 
     query_rows = cur.fetchall()
     for row in query_rows:
