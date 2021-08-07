bcf7b91975077cd08ff5aca08c11b9c092b4ab4b
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-08-07 14:02:55 -0500

    adding doc

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+2-my_filter_states file
 
 
 """
@@ -8,6 +9,8 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
+    """ The file is not executed when imported
+    """
 
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=argv[1], passwd=argv[2],
