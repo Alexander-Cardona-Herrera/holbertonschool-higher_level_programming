be8d15f5d64f521db8520438d0cebf938b9840c4
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-08-08 15:27:36 -0500

    adding task 100

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+model_state file
+
+
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ Class State inherited from Base. """
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all, delete', backref=('state'))
