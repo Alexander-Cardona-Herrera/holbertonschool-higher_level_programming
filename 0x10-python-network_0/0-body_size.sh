d6c662b96c5259f2e0f9f220824fe6c689b56b2c
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-08-19 16:26:04 -0500

    upper cases

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# displaythe size of the body from the response of the request url 
-curl -sI "$1" | grep -i content-length | awk '{print $2}'
+# display the size of the body from the response of the request url 
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
