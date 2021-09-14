1644f80ecf1acd041b97c06c09034c3522858e73
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-09-14 15:00:40 -0500

    adding tsome changes

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,8 +3,7 @@
 const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err, data) => {
-  if (err) {
-    return console.error(err);
-  }
-  console.log(data);
+    if (err) {
+        return console.error(err);
+    }
 });
