7795c36116d0e755e39b6fe81e95b02e7b764db9
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-09-15 09:45:17 -0500

    readding task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,8 +3,8 @@
 const request = require('request');
 
 request(process.argv[2], function (err, response) {
-    if (err) {
-        return console.error(err);
-    }
-    console.log('code: ' + response.statusCode);
+  if (err) {
+    return console.error(err);
+  }
+  console.log('code: ' + response.statusCode);
 });
